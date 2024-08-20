34686ebdd9e8c660dda56ac16401cd58aba63ebe
redMulla <ivassillycharite@gmail.com>
2024-08-20 16:13:11 +0200

    sales summary component initialization

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -5,6 +5,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { useRouter } from "next/navigation";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
+import SalesSummary from "@/components/SalesSummary";
 config.autoAddCss = false;
 
 export default function Home() {
@@ -21,7 +22,8 @@ export default function Home() {
 
   return (
     <div className="h-[100%] w-[100%] grid grid-cols-3 gap-4">
-      <div className="col-span-2 flex items-center justify-center h-full w-full">
+      <div className="col-span-2 h-full w-full">
+        <SalesSummary />
         <h1 className="text-3xl font-bold text-blue-950">Home Page App</h1>
       </div>
       <div className="bg-white border-s border-blue-950"></div>
---
+++ src/components/SalesSummary.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SalesSummary = () => {
+  return <div>SalesSummary</div>;
+};
+
+export default SalesSummary;
